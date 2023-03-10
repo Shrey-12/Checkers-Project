24a0ca3e3811cd1161f8b4578d99dcf84c415414
Shrey-12 <shreya120malik@gmail.com>
2023-03-10 09:52:24 +0530

    Without AI

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame as py
+
+WIDTH,HEIGHT=800,800
+ROWS,COLS=8,8
+SQUARE_SIZE=WIDTH//COLS
+
+SQUARE1=(225,188,96)
+SQUARE2=(106,58,26)
+GOTI1=(9,9,9)
+GOTI2=(205,172,115)
+SELECTOR=( 130,95,49)
+OUTLINE=(255,255,255)
+
+CROWN=py.transform.scale(py.image.load('checkers/crown.png'),(44,25))
